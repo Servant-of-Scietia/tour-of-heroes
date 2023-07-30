52036a59ad231baa5930108840ff204bdfe33048
Samuel Leber <samuel.leber@outlook.com>
2023-07-30 21:36:14 +0200

    test

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -8,15 +8,16 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  constructor(private heroService: HeroService){}
+export class HeroesComponent implements OnInit{
+  constructor(private heroService: HeroService, private messageService: MessageService){}
   heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
   ngOnInit(): void {
     this.getHeroes();
